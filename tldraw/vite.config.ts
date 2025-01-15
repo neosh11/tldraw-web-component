646972592daa9cc87129df775b1bc73a69b0d84e
Neosh <neosh11@gmail.com>
2025-01-15 13:50:02 +1100

    Inject css via js to make the package independent

--- tldraw/vite.config.ts
+++ tldraw/vite.config.ts
@@ -2,34 +2,39 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import typescript from "@rollup/plugin-typescript";
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 export default defineConfig({
-  plugins: [react(), typescript()],
-  define: {
-    // This will replace `process.env` with `{}` during the build
-    "process.env": {},
-  },
-  build: {
-    lib: {
-      // This is the entry point that Vite will use to build the library
-      entry: "./src/index.ts",
-      name: "TldrawWebComponent",
-      fileName: "tldraw-web-component",
+    plugins: [
+        react(),
+        typescript(),
+        cssInjectedByJsPlugin()
+    ],
+    define: {
+        // This will replace `process.env` with `{}` during the build
+        "process.env": {},
     },
-    rollupOptions: {
-      output: [
-        {
-          format: "es",
-          entryFileNames: "[name].esm.js",
-          dir: "dist",
+    build: {
+        lib: {
+            // This is the entry point that Vite will use to build the library
+            entry: "./src/index.ts",
+            name: "TldrawWebComponent",
+            fileName: "tldraw-web-component",
         },
-        {
-          format: "umd",
-          name: "TldrawWebComponent",
-          entryFileNames: "[name].umd.js",
-          dir: "dist",
+        rollupOptions: {
+            output: [
+                {
+                    format: "es",
+                    entryFileNames: "[name].esm.js",
+                    dir: "dist",
+                },
+                {
+                    format: "umd",
+                    name: "TldrawWebComponent",
+                    entryFileNames: "[name].umd.js",
+                    dir: "dist",
+                },
+            ],
         },
-      ],
     },
-  },
 });
